16c93b5c6060c795c54585d85c66dbee9474d5d6
TRZ <ztr8526@gmail.com>
2024-04-26 03:07:24 +0800

    Project3

--- Project3/MatrixKey.c
+++ Project3/MatrixKey.c
@@ -1,37 +1,41 @@
 #include <REGX51.H>
 #include "Delay.h"
 
-unsigned int MatrixKey()
+unsigned char MatrixKey()
 {
-	int KeyNum=0;
-	
-	P1=0xFF;
-	P1_0=0;
-	if(P1_4==0){Delay(10);while(P1_4==0);Delay(10);KeyNum=1;}
-	if(P1_5==0){Delay(10);while(P1_5==0);Delay(10);KeyNum=2;}
-	if(P1_6==0){Delay(10);while(P1_6==0);Delay(10);KeyNum=3;}
-	if(P1_7==0){Delay(10);while(P1_7==0);Delay(10);KeyNum=4;}
-	
-	P1=0xFF;
-	P1_1=0;
-	if(P1_4==0){Delay(10);while(P1_4==0);Delay(10);KeyNum=5;}
-	if(P1_5==0){Delay(10);while(P1_5==0);Delay(10);KeyNum=6;}
-	if(P1_6==0){Delay(10);while(P1_6==0);Delay(10);KeyNum=7;}
-	if(P1_7==0){Delay(10);while(P1_7==0);Delay(10);KeyNum=8;}
-
-	P1=0xFF;
-	P1_2=0;
-	if(P1_4==0){Delay(10);while(P1_4==0);Delay(10);KeyNum=9;}
-	if(P1_5==0){Delay(10);while(P1_5==0);Delay(10);KeyNum=10;}
-	if(P1_6==0){Delay(10);while(P1_6==0);Delay(10);KeyNum=11;}
-	if(P1_7==0){Delay(10);while(P1_7==0);Delay(10);KeyNum=12;}
-
-	P1=0xFF;
-	P1_3=0;
-	if(P1_4==0){Delay(10);while(P1_4==0);Delay(10);KeyNum=13;}
-	if(P1_5==0){Delay(10);while(P1_5==0);Delay(10);KeyNum=14;}
-	if(P1_6==0){Delay(10);while(P1_6==0);Delay(10);KeyNum=15;}
-	if(P1_7==0){Delay(10);while(P1_7==0);Delay(10);KeyNum=16;}
-
-	return KeyNum;
+  unsigned char KeyNumber=0;
+  
+  P1=0xFF;// 1111 1111 全部置高电平默认
+  P1_3=0; // 矩阵按键第一行扫描
+Delay(5);
+  if(P1_7==0){Delay(20);while(P1_7==0);Delay(20);KeyNumber=1;}
+  if(P1_6==0){Delay(20);while(P1_6==0);Delay(20);KeyNumber=5;}
+  if(P1_5==0){Delay(20);while(P1_5==0);Delay(20);KeyNumber=9;}
+  if(P1_4==0){Delay(20);while(P1_4==0);Delay(20);KeyNumber=13;}
+  
+  P1=0xFF;
+  P1_2=0; // 矩阵按键第二行扫描
+  Delay(5);
+  if(P1_7==0){Delay(20);while(P1_7==0);Delay(20);KeyNumber=2;}
+  if(P1_6==0){Delay(20);while(P1_6==0);Delay(20);KeyNumber=6;}
+  if(P1_5==0){Delay(20);while(P1_5==0);Delay(20);KeyNumber=10;}
+  if(P1_4==0){Delay(20);while(P1_4==0);Delay(20);KeyNumber=14;}
+  
+  P1=0xFF;
+  P1_1=0; // 矩阵按键第三行扫描
+  Delay(20);
+  if(P1_7==0){Delay(20);while(P1_7==0);Delay(20);KeyNumber=3;}
+  if(P1_6==0){Delay(20);while(P1_6==0);Delay(20);KeyNumber=7;}
+  if(P1_5==0){Delay(20);while(P1_5==0);Delay(20);KeyNumber=11;}
+  if(P1_4==0){Delay(20);while(P1_4==0);Delay(20);KeyNumber=15;}
+  
+  P1=0xFF;
+  P1_0=0; // 矩阵按键第四行扫描
+  Delay(20);
+  if(P1_7==0){Delay(20);while(P1_7==0);Delay(20);KeyNumber=4;}
+  if(P1_6==0){Delay(20);while(P1_6==0);Delay(20);KeyNumber=8;}
+  if(P1_5==0){Delay(20);while(P1_5==0);Delay(20);KeyNumber=12;}
+  if(P1_4==0){Delay(20);while(P1_4==0);Delay(20);KeyNumber=16;}
+  
+  return KeyNumber;
 }
--- Project3/main.c
+++ Project3/main.c
@@ -3,43 +3,57 @@
 #include "Timer1.h"
 #include "Delay.h"
 #include "MatrixKey.h"
-
-char dig[]={0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x98};
+int i=0;
+int press=0;
+char dig[]={0x3f, 0x06, 0x5b, 0x4f,0x66, 0x6d, 0x7d, 0x07,0x7f, 0x6f, 0x77, 0x7c,0x39, 0x5e, 0x79, 0x71};
+char show[]={0,0,0,0,0,0,0,0};
 void main()
 {
-	Timer0_Init();
+//	Timer0_Init();
 	Timer1_Init();
-	P2=0x07;
-	//P0=0x01;
+	P2=0x00;
+	P0=0x3f;
 	while(1)
 	{
-		int key=MatrixKey();
+		char key=MatrixKey();
 		if(key)
 		{
-			key=MatrixKey();
-			while(key);
-			P0=0x01;
+			
+
+			int tmp=7;
+			while(tmp>=0)
+			{
+				show[tmp]=show[tmp-1];
+				tmp--;
+			}
+
+
+			show[0]=dig[key];
+			press++;
+			if(press>7)
+				press=0;
 		}
+		
 	}
 }
 
-unsigned int count0=0;
-void Timer0_LED()	interrupt 1
-{
-	TL0 = 0x18;				//设置定时初始值
-	TH0 = 0xFC;				//设置定时初始值
-	count0++;
-	if(count0%200==0)
-		P3_0=~P3_0;
-	if(count0%300==0)
-		P3_1=~P3_1;
-	if(count0%500==0)
-		P3_2=~P3_2;
-	if(count0%1000==0)
-		P3_3=~P3_3;
-	if(count0==3000)
-		count0=0;
-}
+//unsigned int count0=0;
+//void Timer0_LED()	interrupt 1
+//{
+//	TL0 = 0x18;				//设置定时初始值
+//	TH0 = 0xFC;				//设置定时初始值
+//	count0++;
+//	if(count0%200==0)
+//		P3_0=~P3_0;
+//	if(count0%300==0)
+//		P3_1=~P3_1;
+//	if(count0%500==0)
+//		P3_2=~P3_2;
+//	if(count0%1000==0)
+//		P3_3=~P3_3;
+//	if(count0==3000)
+//		count0=0;
+//}
 
 unsigned int count1=0;
 
@@ -52,6 +66,11 @@ void Timer1()	interrupt 3
 	{
 		count1=0;
 		P2--;
+		P0=0;
+		P0=show[i];
+		i++;
+		if(i>7)
+			i=0;
 		if(P2<0)
 			P2=0x07;
 	}
